84c6f0207ce5d13a70bdb68258d6106f122f5da8
Gergo Ertli <egergo@gmail.com>
2016-05-16 13:48:10 +0300

    Error handling of loading reviews

--- src/reducers/reviews.js
+++ src/reducers/reviews.js
@@ -4,21 +4,24 @@ import {LOAD} from '../actions/reviews'
 
 export default handleActions({
   [LOAD]: (state, action) => {
-    if (action.error) return state
+    if (action.error) return {...state, loading: false, error: action.payload.message}
     if (!action.payload) {
       return {
         ...state,
-        loading: true
+        loading: true,
+        error: null
       }
     }
     const reviews = action.payload
     return {
       ...state,
       loading: false,
+      error: null,
       reviews
     }
   }
 }, {
   reviews: [],
-  loading: false
+  loading: false,
+  error: null
 })
