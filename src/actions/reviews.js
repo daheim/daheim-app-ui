736f169a1c025bf826f8c2b3d4355f992c08d212
Gergo Ertli <egergo@gmail.com>
2016-05-24 09:57:54 +0300

    Removed old review action and reducer

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -1,11 +1,3 @@
-import {createAction} from 'redux-actions'
-import api from '../api_client'
-
-export const LOAD = 'reviews/load'
-export const load = createAction(LOAD, () => {
-  return api.get('/encounters')
-})
-
 export const SEND_REVIEW = 'review.sendReview'
 export const sendReview = (body) => {
   return {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 
 import messages from './messages'
-import reviews from './reviews'
 import profile from './profile'
 import users from './users'
 import lessons from './lessons'
@@ -10,7 +9,6 @@ import {liveReducer} from '../live'
 
 export default combineReducers({
   messages,
-  reviews,
   profile,
   users,
   lessons,
