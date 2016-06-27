ff10c5d3a7a34720103a00b85cc5b7d9602d68b1
Gergo Ertli <egergo@gmail.com>
2016-06-27 10:33:06 +0300

    Changed ready switch layout

--- src/components/ready/ReadySwitch.js
+++ src/components/ready/ReadySwitch.js
@@ -1,8 +1,7 @@
 import React, {PropTypes, Component} from 'react'
 import {connect} from 'react-redux'
 import CircularProgress from 'material-ui/CircularProgress'
-import TextField from 'material-ui/TextField'
-import PowerSettingsNew from 'material-ui/svg-icons/action/power-settings-new'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import {ready as setReady} from '../../actions/live'
 import style from './ReadySwitch.style'
@@ -19,7 +18,7 @@ class Connecting extends Component {
       <div style={{display: 'flex', alignItems: 'center'}}>
         <div style={{margin: 20}}><CircularProgress /></div>
         <div style={{margin: 8}}>
-          <div className={style.connecting}>Connecting to Daheim network. Please wait a few moments...</div>
+          <div className={style.connecting}>Verbinding mit Daheim Netzwerk wird hergestellt. Bitte warten!</div>
           {error ? (
             <div className={style.error}>Fehler: {error}</div>
           ) : null}
@@ -38,64 +37,62 @@ class ReadySwitch extends Component {
     readyTopic: PropTypes.string
   }
 
-  state = {
-    topic: this.props.readyTopic || ''
-  }
+  state = {busy: false}
 
   handleReadyChange = async (e) => {
     try {
-      await this.props.setReady({ready: e.target.checked, topic: this.state.topic})
+      await this.props.setReady({ready: e.target.checked})
     } catch (err) {
       alert(err.message)
     }
-  }
-
-  handleTopicChange = (e) => {
-    this.setState({topic: e.target.value})
   }
 
   goOnline = async () => {
-    if (!this.state.topic) this.setState({topic: 'keine Theme'})
+    if (this.state.busy) return
+    this.setState({busy: true})
 
     try {
-      await this.props.setReady({ready: true, topic: this.state.topic || 'keine Theme'})
+      await this.props.setReady({ready: true})
     } catch (err) {
-      alert(err.message)
+      alert('Fehler: ' + err.message)
+    } finally {
+      this.setState({busy: false})
     }
   }
 
-  goOffline = async () => {
+  goOffline = async (e) => {
+    e.preventDefault()
+
+    if (this.state.busy) return
+    this.setState({busy: true})
+
     try {
       await this.props.setReady({ready: false})
     } catch (err) {
-      alert(err.message)
+      alert('Fehler: ' + err.message)
+    } finally {
+      this.setState({busy: false})
     }
   }
 
   render () {
     const {ready, connected} = this.props
-    const {topic} = this.state
+    const {busy} = this.state
 
     if (!connected) return <Connecting {...this.props} />
 
-    if (!ready) {
+    if (!ready && !busy) {
       return (
-        <div style={{display: 'flex', alignItems: 'center', margin: 20}}>
-          <PowerSettingsNew color='#a51515' hoverColor='crimson' style={{cursor: 'pointer', width: 128, height: 128}} onClick={this.goOnline} />
-          <div style={{flex: '1 1 auto', marginLeft: 20}}>
-            <div style={{fontFamily: 'Lato, sans-serif'}}><b>Du bist noch nicht bereit.</b> Bitte wähle eine theme und klicke die rote Taste!</div>
-            <TextField value={topic} hintText='Theme' onChange={this.handleTopicChange} />
-          </div>
+        <div style={{textAlign: 'center', margin: '40px 20px'}}>
+          <RaisedButton label={'Los geht\'s - Gesprächpartner suchen'} primary onClick={this.goOnline} />
         </div>
       )
     } else {
       return (
         <div style={{display: 'flex', alignItems: 'center', margin: 20}}>
-          <PowerSettingsNew color='forestgreen' hoverColor='olivedrab' style={{cursor: 'pointer', width: 128, height: 128}} onClick={this.goOffline} />
-          <div style={{flex: '1 1 auto', marginLeft: 20}}>
-            <div style={{fontFamily: 'Lato, sans-serif', lineHeight: '150%'}}><b>Du bist bereit.</b> Deine Theme Heute ist: <i>{topic}</i></div>
-            <div style={{fontFamily: 'Lato, sans-serif', lineHeight: '150%'}}>Gleich kannst du mit einem/er Sprachcoach/In sprechen.</div>
-            <div style={{fontFamily: 'Lato, sans-serif', lineHeight: '150%'}}>Bitte warte auf diese Seite.</div>
+          <div style={{margin: 20}}><CircularProgress /></div>
+          <div style={{margin: 8}}>
+            <div>Es werden passende Gesprächspartner gesucht. <a href='#' onClick={this.goOffline}>abbrechen</a></div>
           </div>
         </div>
       )
